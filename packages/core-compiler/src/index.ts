8b3df088285876b72d6b2438d84e556ab50fad54
Yashu Bansal <yashubansal000@users.noreply.github.com>
2026-03-27 20:34:24 +0530

    Isolate shared package and root files

--- packages/core-compiler/src/index.ts
+++
@@ -1,7 +0,0 @@
-export { compile } from './compiler';
-export { lex } from './lexer';
-export { Parser } from './parser';
-export { TACGenerator } from './tacGenerator';
-export { toQuadruples, toTriples, toSSA } from './tacFormats';
-export { samples } from './samples';
-export type { TACFormat } from './tacFormats';
--- packages/visualizers/src/index.ts
+++
@@ -1,8 +0,0 @@
-export { ASTViewer } from './ASTViewer';
-export { CFGViewer } from './CFGViewer';
-export { SymbolTableViewer } from './SymbolTableViewer';
-export { DataFlowViewer } from './DataFlowViewer';
-export { MemoryLayoutViewer } from './MemoryLayoutViewer';
-export { buildSymbolTable } from './symbolTable';
-export { analyzeDataFlow } from './dataFlowAnalysis';
-export { analyzeMemoryLayout } from './memoryLayout';
